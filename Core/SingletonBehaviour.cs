e5602718f2e6376df937a6a886be546eab496bfc
Alvin <bluexo@qq.com>
2017-07-12 15:16:37 +0800

    SingletonBehaviour comment improved!

--- Core/SingletonBehaviour.cs
+++ Core/SingletonBehaviour.cs
@@ -1,7 +1,7 @@
 ﻿namespace UnityEngine
 {
     /// <summary>
-    /// MonoBehaviour Singleton , if subclass has not attach to GameObject , will be create root GameObject than has a name equals <see cref="typeof(T).FullName"/>
+    /// MonoBehaviour Singleton , if subclass has not attach to GameObject , will be create root GameObject that has a name equals <see cref="typeof(T).FullName"/>
     /// MonoBehaviour 单例基类，如果子类没有附加到物体上，将会自动创建全局对象 , 以  <see cref="typeof(T).FullName"/> 来命名
     /// </summary>
     /// <typeparam name="T"></typeparam>
@@ -25,7 +25,7 @@
         private static T instance;
 
         /// <summary>
-        /// 如果子类重写Awake方法，必须调用基类的Awake
+        /// 如果子类重写 Awake() 方法，必须调用基类的 Awake()
         /// if subclass override Awake() , must be call base.Awake() 
         /// </summary>
         protected virtual void Awake()
@@ -34,8 +34,10 @@
                 instance = this as T;
                 DontDestroyOnLoad(gameObject);
             } else {
-                if (instance && !instance.Equals(this))
+                if (instance && !instance.Equals(this)) {
                     Destroy(instance);
+                    Debug.LogErrorFormat("The {0} not allow running multiple instances , the redundant will be Destroy!", typeof(T).FullName);
+                }
             }
         }
     }
