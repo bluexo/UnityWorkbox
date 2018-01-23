262c4c00681ac45753052cc7a7da4f557f48e61a
Alvin <bluexo@hotmail.com>
2018-01-23 21:42:33 +0800

    对象池

---
+++ Utility/ObjectPool.cs
@@ -0,0 +1,136 @@
+﻿using Arthas.Common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arthas.Common
+{
+    public interface IResetable<T> where T : Component
+    {
+        event System.Action<T> ResetEvent;
+
+        bool IsCollected { get; }
+
+        void ResetObject();
+    }
+
+    public class ObjectPool<TComponent> : SingletonBehaviour<ObjectPool<TComponent>>
+        where TComponent : Component, IResetable<TComponent>
+    {
+        public IDictionary<int, Queue<TComponent>> ObjectQueue { get { return objectQueue; } }
+        private readonly Dictionary<int, Queue<TComponent>> objectQueue = new Dictionary<int, Queue<TComponent>>();
+        private readonly WaitForEndOfFrame waitForEnd = new WaitForEndOfFrame();
+        private readonly WaitForSeconds waitForCollect = new WaitForSeconds(10f);
+
+        [SerializeField] protected PoolObjectConfig objectArray;
+        [SerializeField, Range(1, 20)] protected int initCount = 10;
+        [SerializeField, Range(1, 100)] protected int maxOverload = 16;
+
+        protected IEnumerator Start()
+        {
+            if (objectArray == null || objectArray.Items == null)
+            {
+                Debug.LogError("Cannot found prefab");
+                yield break;
+            }
+            for (var i = 0; i < objectArray.Items.Length; i++)
+            {
+                var item = objectArray.Items[i];
+                yield return Spawn(item);
+            }
+            StartCoroutine(CollectPollingAsync());
+        }
+
+        protected IEnumerator CollectPollingAsync()
+        {
+            while (true)
+            {
+                yield return waitForCollect;
+                foreach (var pair in objectQueue)
+                {
+                    if (pair.Value.Count == 0) continue;
+                    while (pair.Value.Count > maxOverload)
+                    {
+                        var comp = pair.Value.Dequeue();
+                        if (comp && !comp.gameObject.activeSelf)
+                            Destroy(comp.gameObject);
+                        yield return waitForEnd;
+                    }
+                }
+            }
+        }
+
+        protected IEnumerator Spawn(PoolObjectInfo item)
+        {
+            if (item == null) yield break;
+            if (!objectQueue.ContainsKey(item.id)) objectQueue.Add(item.id, new Queue<TComponent>());
+            for (var j = 0; j < initCount; j++)
+            {
+                var prefab = objectArray.Items[item.id].prefab;
+                var comp = Instantiate(prefab).GetComponent<TComponent>();
+                if (!comp)
+                {
+                    Debug.LogErrorFormat("Cannot found [{0}] from object pool!", typeof(TComponent));
+                    continue;
+                }
+                comp.transform.SetParent(transform);
+                comp.gameObject.SetActive(false);
+                objectQueue[item.id].Enqueue(comp);
+                yield return waitForEnd;
+            }
+        }
+
+        protected IEnumerator CollectAsync(int id, IEnumerable<TComponent> bullets, YieldInstruction yieldInstruction)
+        {
+            if (yieldInstruction != null) yield return yieldInstruction;
+            if (!objectQueue.ContainsKey(id))
+                objectQueue.Add(id, new Queue<TComponent>());
+            foreach (var obj in bullets)
+            {
+                yield return waitForEnd;
+                obj.ResetObject();
+                obj.gameObject.SetActive(false);
+                objectQueue[id].Enqueue(obj);
+            }
+        }
+
+        public void Put(int id, TComponent comp, bool disable = true)
+        {
+            if (!objectQueue.ContainsKey(id))
+                objectQueue.Add(id, new Queue<TComponent>());
+            if (disable && comp && comp.gameObject) comp.gameObject.SetActive(false);
+            comp.ResetObject();
+            objectQueue[id].Enqueue(comp);
+        }
+
+        public void DelayPut(int id, TComponent obj, YieldInstruction waitFor)
+        {
+            StartCoroutine(instance.CollectAsync(id, new TComponent[] { obj }, waitFor));
+        }
+
+        public TComponent Get(int id, bool active = true)
+        {
+            if (!objectQueue.ContainsKey(id))
+                objectQueue.Add(id, new Queue<TComponent>());
+            if (objectQueue[id].Count < initCount)
+            {
+                var item = objectArray.Items.FirstOrDefault(o => o.id == id);
+                if (item != null) StartCoroutine(Spawn(item));
+            }
+            var obj = objectQueue[id].Dequeue();
+            if (active && obj && obj.gameObject) obj.gameObject.SetActive(true);
+            return obj;
+        }
+
+        public void Clear()
+        {
+            foreach (var array in objectQueue.Values)
+            {
+                foreach (var obj in array)
+                    if (obj) Destroy(obj.gameObject);
+            }
+            objectQueue.Clear();
+            StopAllCoroutines();
+        }
+    }
+}
---
+++ Utility/PoolObjectConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arthas.Common
+{
+    [System.Serializable]
+    public class PoolObjectInfo
+    {
+        public int id;
+        public string name;
+        public GameObject prefab;
+    }
+
+    [CreateAssetMenu(menuName = "Configs/ObjectPoolConfig")]
+    public class PoolObjectConfig : VisualConfig<PoolObjectInfo> { }
+}
