9ac519ad472c1c0fb519137bbbe939460d4648c8
Alvin <bluexo@hotmail.com>
2018-05-17 19:04:21 +0800

    日志和心跳开关

--- Network/Networker.cs
+++ Network/Networker.cs
@@ -53,8 +53,8 @@ namespace Arthas.Network
         public static NetworkStatus NetworkStatus { get; private set; }
         public static Func<object> HeartbeatCommandGetter { get; set; }
 
-        public readonly ByteBuf buf = new ByteBuf(1024);
-        public const short HeartbeatCommand = 0;
+        public ByteBuf buf;
+        public const short HeartbeatCommand = 0, MaxBuffSize = 4096;
 
         public static bool IsLittleEndian
         {
@@ -77,7 +77,10 @@ namespace Arthas.Network
         [SerializeField]
         private float connectTimeout = 10f, heartbeatInterval = 12f, pingInterval = 3f;
         [SerializeField]
-        private bool isLittleEndian = true, useSSL = false;
+        private bool isLittleEndian = true,
+            useSSL = false,
+            enableLogging = true,
+            enableHeartbeat = true;
         [SerializeField]
         private int maxRetryCount = 3;
 
@@ -165,8 +168,9 @@ namespace Arthas.Network
 
         protected void Connect()
         {
-            if (isConnecting) return;
+            if (isConnecting || IsConnected) return;
             isConnecting = true;
+            buf = new ByteBuf(MaxBuffSize);
             prevConnectTime = Time.time;
             if (connector != null) connector.Connect(NetworkAddress.ip, NetworkAddress.port);
             InvokeStatusEvent(NetworkStatus.Connecting);
@@ -206,7 +210,7 @@ namespace Arthas.Network
             {
                 yield return heartbeatWaitFor;
 
-                if (connector.IsConnected)
+                if (enableHeartbeat && connector.IsConnected)
                 {
                     if (HeartbeatCommandGetter != null) Send(HeartbeatCommandGetter());
                     else Send(0);
@@ -278,7 +282,8 @@ namespace Arthas.Network
                     msgQueue.Enqueue(msgs[i]);
                     if (msgQueue.Count > byte.MaxValue) msgQueue.Dequeue();
 #if UNITY_EDITOR || DEVELOPMENT_BUILD
-                    Debug.LogFormat("<color=blue>[TCPNetwork]</color> [Receive] << CMD:{0},TIME:{1}", msgs[i].Command, DateTime.Now);
+                    if (Instance.enableLogging)
+                        Debug.LogFormat("<color=blue>[TCPNetwork]</color> [Receive] << CMD:{0},TIME:{1}", msgs[i].Command, DateTime.Now);
 #endif
                 }
             }
@@ -331,7 +336,8 @@ namespace Arthas.Network
                 connector.Send(buffer);
                 prevSendTime = Time.time;
 #if UNITY_EDITOR || DEVELOPMENT_BUILD
-                Debug.LogFormat("<color=cyan>[TCPNetwork]</color> [Send] >> CMD:{0},TIME:{1}", cmd, DateTime.Now);
+                if (Instance.enableLogging)
+                    Debug.LogFormat("<color=cyan>[TCPNetwork]</color> [Send] >> CMD:{0},TIME:{1}", cmd, DateTime.Now);
 #endif
             }
             catch (Exception ex)
