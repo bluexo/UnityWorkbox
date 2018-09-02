6a5c1c3dc2b24211e2c8c0b553195b733d9066b1
赵永 <bluexo@hotmail.com>
2018-09-02 20:59:59 +0800

    AutoIndexAttribute & ObjRefAttribute

---
+++ Core/Attributes/AutoIndexAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace Arthas
+{
+    public class AutoIndexAttribute : PropertyAttribute
+    {
+        public int StartIndex { get; set; }
+    }
+}
---
+++ Core/Attributes/ObjRefAttribute.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+namespace Arthas
+{
+    using UnityEditor;
+
+#if UNITY_EDITOR
+
+    [CustomPropertyDrawer(typeof(ObjRefAttribute))]
+    public class ObjRefAttributePropertyDrawer : PropertyDrawer
+    {
+        private float height = 32f;
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) => height;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType != SerializedPropertyType.Integer
+                && property.propertyType != SerializedPropertyType.String) return;
+
+            var labelRect = new Rect(position.x, position.y, position.width * 3 / 4, height / 2);
+            var selectorRect = new Rect(position.x + position.width * 3 / 4 + 10f, position.y, position.width * 1 / 6, height / 2);
+
+            if (property.propertyType == SerializedPropertyType.Integer)
+            {
+                property.intValue = EditorGUI.IntField(labelRect, property.displayName, property.intValue);
+            }
+            else if (property.propertyType == SerializedPropertyType.String)
+            {
+                property.stringValue = EditorGUI.TextField(labelRect, property.displayName, property.stringValue);
+            }
+
+            if (GUI.Button(selectorRect, "⊙"))
+            {
+
+            }
+        }
+    }
+
+#endif
+
+    public class ObjRefAttribute : PropertyAttribute
+    {
+        public Type RefType { get; set; }
+        public string FieldName { get; set; }
+
+        public ObjRefAttribute(Type refType, string fieldName)
+        {
+            RefType = refType;
+            FieldName = fieldName;
+        }
+    }
+}
